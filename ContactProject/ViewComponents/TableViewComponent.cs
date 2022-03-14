65b7f411535d8be3ba6cb2bb7b5dc72ec5dcca30
mihran-w <mihranw@gmail.com>
2022-03-14 18:30:44 +0330

    Adding Files by View Components

---
+++ ContactProject/ViewComponents/TableViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactProject.ViewComponents
+{
+    public class TableViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult(View("Table",DataBase.DataBase.logins));
+        }
+    }
+}
